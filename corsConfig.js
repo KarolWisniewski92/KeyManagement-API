cd90b7bde3f3c339b394812fce924717b35bb18f
Karol Wiśniewski <karolwisniewski92@gmail.com>
2022-03-15 22:59:53 +0100

    Transferred originDirection to .env file.

--- corsConfig.js
+++ corsConfig.js
@@ -1,8 +1,10 @@
 const cors = require('cors');
 
+const originDirection = `${process.env.CLIENT_IP_ADRESS}:${process.env.CLIENT_PORT}`
+
 const corsConfig = (app) => {
     app.use(cors({
-        origin: [`http://192.168.0.120:${process.env.CLIENT_PORT}`, `http://localhost:${process.env.CLIENT_PORT}`],
+        origin: [originDirection, `http://localhost:${process.env.CLIENT_PORT}`],
         credentials: true
     }));
 }
